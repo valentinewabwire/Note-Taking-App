f5da904ed4c4e96571b6dde17025e891ed6ef40a
Valentine Wabwire <valentine.w.maita@gmail.com>
2022-11-18 11:17:03 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <div className="header-text"></div>;
+}
+
+export default Header;
---
+++ src/components/NoteInput.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoteInput() {
+  return <div>NoteInput</div>;
+}
+
+export default NoteInput;
